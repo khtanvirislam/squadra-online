fa2e661040e15dcb1cf4442e688027fa0c922355
Khtanvirislam <khtanvir2020@gmail.e.com>
2023-12-13 22:31:50 +0600

    add files are ready to execute

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const MainLayout = () => {
+  return (
+    <div>
+      <div>
+        <h2 className="text-3xl">This is a Navbar</h2>
+      </div>
+    </div>
+  );
+};
---
+++ src/Router/router.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import { MainLayout } from "../Layout/MainLayout";
+import { Home } from "../Pages/Home/Home";
+
+export const myCreatedRoute = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
