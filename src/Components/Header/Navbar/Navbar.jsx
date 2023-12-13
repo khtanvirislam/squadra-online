5b896989dd501743b9824f7709d806ab4fa40a31
Khtanvirislam <khtanvir2020@gmail.e.com>
2023-12-14 00:34:49 +0600

    nav bar is updated

--- src/Components/Header/Navbar/Navbar.jsx
+++ src/Components/Header/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { Logo } from "./Logo";
+import { Logo } from "../Logo/Logo";
 
 export const NavBar = () => {
   return (
